918686eec1a37f1a6135bacb973bd65f8974bd4d
shams-farooq <83645760+shams-farooq@users.noreply.github.com>
2022-01-27 14:58:53 +0500

    Network Class Implemented

---
+++ app/src/main/java/com/example/handlusernew/network/URLApi.kt
@@ -0,0 +1,56 @@
+@file:Suppress("SpellCheckingInspection", "unused", "FunctionName")
+
+package com.example.handlusernew.network
+
+
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import org.json.JSONObject
+
+
+
+
+@Suppress("UNUSED_PARAMETER")
+object URLApi {
+
+
+    private val TAG = URLApi::class.java.toString()
+
+    const val SOCKET_URL = "http://45.56.122.34:1028"
+    public const val BaseUrl =
+        "http://45.56.122.34/bakeryapp/api/" //Base URL here
+    private var path: String = ""
+    private var params: JSONObject = JSONObject()
+    var method: NetworkMethod = NetworkMethod.GET
+    fun link(): String {
+        return BaseUrl + path
+    }
+
+    fun param(): JSONObject {
+        return params
+    }
+
+    final fun paramHas(): HashMap<String, Any>? {
+        return Gson().fromJson<HashMap<String, Any>>(
+            param().toString(), object : TypeToken<HashMap<String?, Any?>?>() {}.type
+        )
+    }
+
+    final fun paramHashMap(): HashMap<*, *> {
+        return Gson().fromJson(params.toString(), HashMap::class.java)
+    }
+
+
+
+
+    fun getUser(user_id: String): URLApi {
+        method = NetworkMethod.POST
+        path = "auth/user"
+        params = JSONObject()
+        params.put("user_id", user_id)
+        return this
+    }
+
+}
+
+
